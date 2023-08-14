0e2dd1c95da3989ab84c64a1781cc962905adfbc
Paulo Gutiérrez <PCGP22@gmail.com>
2023-08-13 21:59:32 -0600

    Deploy Front

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 3000,
+    port:3000,
     open: '/countries',
   }
 })
